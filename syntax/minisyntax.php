7d5a188b27f8a1a87d253b0f4db11d49b5e97376
Nicolas Gerard <gerardnico@gerardnico.com>
2015-10-27 21:09:26 +0100

    Build the panel with list group

--- syntax/minisyntax.php
+++ syntax/minisyntax.php
@@ -54,7 +54,7 @@ class syntax_plugin_minimap_minisyntax extends DokuWiki_Syntax_Plugin {
         $pages = $this->getPagesOfNamespace($currentNameSpace);
 
         // Cache the values
-        return array($state,$pages,$parameters);
+        return array($state,$pages,$parameters,$currentNameSpace);
     }
 
 
@@ -66,12 +66,81 @@ class syntax_plugin_minimap_minisyntax extends DokuWiki_Syntax_Plugin {
         // There is other mode such as metadata where you can output data for the headers (Not 100% sure)
         if ($mode == 'xhtml') {
 
-            list($state, $pages, $parameters) = $data;
+            list($state, $pages, $parameters, $currentNameSpace) = $data;
 
             switch ($state) {
 
                 case DOKU_LEXER_SPECIAL :
-                    $renderer->doc .= '<P>Number of pages: '.count($pages).'</P>';
+
+                    // Set the two possible home page for the namespace
+                    // with the start conf of the name of the last map
+                    global $conf;
+                    $parts = explode(':', $currentNameSpace);
+                    $lastContainingNameSpace = $parts[count($parts)-1];
+                    $homePageNamespace=$currentNameSpace.':'.$lastContainingNameSpace;
+                    $startConf = $conf['start'];
+                    $startPageNamespace=$currentNameSpace.':'.$startConf;
+
+                    // Build the list of page
+                    $miniMapList = '<div class="list-group">';
+                    foreach ($pages as $page) {
+                        // page names
+                        $name = noNSorNS($page['id']);
+                        if(useHeading('navigation')) {
+                            // get page title
+                            $title = p_get_first_heading($page['id'], METADATA_RENDER_USING_SIMPLE_CACHE);
+                            if($title) {
+                                $name = $title;
+                            }
+                            global $ID;
+                            $active='';
+                            if ($ID==$page['id']) {
+                                $active='active';
+                            }
+
+                            $print = true;
+                            if ($page[id] == $page['ns'].':'.$page['ns']) {
+                                $print=false;
+                                $homePageFound=true;
+                            } else if ($page[id] == $page['ns'].':'.$startConf) {
+                                $print=false;
+                                $startPageFound=true;
+                            } else if ($page[id] == $page['ns'].':'.$conf['sidebar']){
+                                $print=false;
+                            };
+
+                            if ($print) {
+                                $miniMapList .= tpl_link(
+                                    wl($page['id']),
+                                    $name,
+                                    'class="list-group-item ' . $active . '" title="' . $name . '"',
+                                    $return = true
+                                );
+                            }
+
+
+                        }
+
+                    }
+                    $miniMapList .= '</div>'; // End list-group
+
+                    // Build the panel header
+                    $miniMapPanel = '<div class="panel panel-default">';
+                    if ($homePageFound) {
+                        $startId = $homePageNamespace;
+                    } else {
+                        if ($startPageFound) {
+                            $startId = $startPageNamespace;
+                        } else {
+                            $panelHeaderContent = 'No Home Page found';
+                        }
+                    }
+                    if (!$panelHeaderContent) {
+                        $panelHeaderContent = tpl_link(wl($startId), tpl_pagetitle($startId,true), 'title="' . $startId . '"',$return=true);
+                    }
+                    $miniMapPanel .= '<div class="panel-heading">'.$panelHeaderContent.'  <span class="label label-primary">'.count($pages).' pages</span></div>';
+                    //$miniMapPanel .= '<div class="panel-body">An optional text</div>';
+                    $renderer->doc .= $miniMapPanel.$miniMapList.'</div>';
                     break;
             }
 
