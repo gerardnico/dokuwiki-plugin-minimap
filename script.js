d33bbd8d56163362a29b2bd9c13d1633a4ed1041
Nicolas GERARD <gerardnico@gmail.com>
2019-03-13 22:16:42 +0100

    Script error

--- script.js
+++ script.js
@@ -3,10 +3,10 @@
 var minimap = {
     minilist: function () {
         console.log("Mini list started");
-        $pages = jQuery("#minimap__plugin  .list-group-item")
+        var $pages = jQuery("#minimap__plugin  .list-group-item");
         $pages.map(a => console.log($pages[a].innerText));
     }
-}
+};
 
 
 // window.addEventListener("load", function() {
