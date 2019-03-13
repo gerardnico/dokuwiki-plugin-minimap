19d1c11b7924987006eab60f5d076b699ad7f4c3
Nicolas GERARD <gerardnico@gmail.com>
2019-03-13 21:08:37 +0100

    Disable the load of the experimental code

--- script.js
+++ script.js
@@ -5,11 +5,10 @@ var minimap = {
         console.log("Mini list started");
         $pages = jQuery("#minimap__plugin  .list-group-item")
         $pages.map(a => console.log($pages[a].innerText));
-
     }
 }
 
 
-window.addEventListener("load", function() {
-    minimap.minilist();
-});
+// window.addEventListener("load", function() {
+//     minimap.minilist();
+// });
